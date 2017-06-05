61a6a9081866dffb91d07fd974bed49f7cd8b7a5
Aos <aosdab@gmail.com>
2017-06-05 13:39:06 -0500

    Done: Allonge ch5 and Exercism anagram

--- Practice/exercism/javascript/anagram/anagram.js
+++ Practice/exercism/javascript/anagram/anagram.js
@@ -4,24 +4,63 @@
  * Problem 11: Anagram
  * 
  * Solution by Aos
- * 6/XX/2017
+ * 6/04/2017
 **/
 
-const Anagram = function() {}
-
-Anagram.prototype.matches = function(array) {
-  let word = ['w', 'o', 'r', 'd', 's'];
-
-  let match = 'word'.split('');
-
-  match.forEach((e) => {
-    if (e in word) {
-      let index = word.indexOf(e);
-      word.splice(e, 1);
-    }
+// Helper function -- tallies letters and returns an object
+function tallyLetters(word) {
+  let letterTally = {};
+  word = word.split('');
+  word.forEach((letter) => {
+    letterTally.hasOwnProperty(letter) ? letterTally[letter]++ : letterTally[letter] = 1
   })
-  console.log(word);
+  return letterTally;
 }
 
+const Anagram = function(test) {
+  this.test = test.toLowerCase();
+  this.testTallied = tallyLetters(this.test);
+}
 
-module.exports = Angram;
+Anagram.prototype.matches = function(array) {
+
+  let result = [];
+  let newArray = [];
+
+  // Convert to array using spread operator
+  if (!Array.isArray(array)) {
+    newArray = [...arguments];
+  }
+  else {
+    newArray = array;
+  }
+
+  // Need to use arrow fns to allow lexical `this` binding
+  newArray.forEach((word) => {
+    let lowercaseWord = word.toLowerCase();
+
+    // Tallies letter count in each word
+    let tallied = tallyLetters(lowercaseWord);
+
+    // Check for identical words
+    if (lowercaseWord === this.test) {
+      return;
+    }
+
+    // First pass, remove words not of the same length
+    if (lowercaseWord.length !== this.test.length) {
+      return;
+    }
+    
+    // Second pass, check for correct amount of letters in each word
+    for (const letter in tallied) {
+      if (this.testTallied[letter] !== tallied[letter]) {
+        return;
+      }
+    }
+    result.push(word);
+  })
+  return result;
+}
+
+module.exports = Anagram;
