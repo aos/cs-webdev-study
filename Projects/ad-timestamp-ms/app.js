60351d1102498a6699adc0dba7d3a3d60d1e877c
Aos <aosdab@gmail.com>
2017-06-09 20:12:12 -0500

    Fixing port + adding start script

--- Projects/ad-timestamp-ms/app.js
+++ Projects/ad-timestamp-ms/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
