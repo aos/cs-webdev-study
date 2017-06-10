85b0909590c1cd4cbcebb674e485b09e1ad8b048
Aos <aosdab@gmail.com>
2017-06-09 20:02:06 -0500

    Reworking static files (AGAIN)

--- Projects/ad-timestamp-ms/app.js
+++ Projects/ad-timestamp-ms/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
