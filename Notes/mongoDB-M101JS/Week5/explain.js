8be0118c88f78a5b73a21943026278d8c9281b00
Aos <aosdab@gmail.com>
2017-06-27 23:33:50 -0500

    M101JS Wk5

---
+++ Notes/mongoDB-M101JS/Week5/explain.js
@@ -0,0 +1,13 @@
+/**
+ * Explain
+
+ Does not bring back the database query.
+ Does not actually return any data, it only replicates the method. Returns an explainable object.
+**/
+db.example.explain();
+
+// You can also call explain on a cursor
+
+// Execution stats mode
+// All plans execution
+
