c012434ca658567da9396f4d47e7811e1fd45c7a
Aos <aosdab@gmail.com>
2017-06-28 04:23:42 -0500

    Done M101JS Wk5

--- Notes/mongoDB-M101JS/Week5/explain.js
+++ Notes/mongoDB-M101JS/Week5/explain.js
@@ -8,6 +8,11 @@ db.example.explain();
 
 // You can also call explain on a cursor
 
-// Execution stats mode
-// All plans execution
+// Explain options:
+
+// 1. Execution stats mode - provides stats for the winning plan
+db.example.explain('executionStats');
+
+// 2. All Plans Execution mode - does what the query optimizer does periodically. Runs all possible indexes that could be used, in parallel, then makes the decision about which one is fastest, then remembers which one is fastest and runs that index.
+// Shows execution stats for ALL plans
 
