9d803c2f6a69db54f28d6ce7c922e9af43dd8e36
Aos <aosdab@gmail.com>
2017-08-30 00:32:40 -0400

    Added 2.7

--- Notes/CTCI/02-linked-lists/7.js
+++ Notes/CTCI/02-linked-lists/7.js
@@ -24,7 +24,81 @@ const intersection = (l1, l2) => {
     }
     p2 = p2.next;
   }
-  return false;
+  return null;
+}
+
+const length = (l1) => {
+  counter = 0;
+  while (l1 !== null) {
+    counter++;
+    l1 = l1.next;
+  }
+  return {
+    counter,
+    l1;
+  }
+}
+
+// Without hash table
+const intsec = (l1, l2) => {
+  let { counter: len1 } = length(l1);
+  let { counter: len2 } = length(l2);
+  let diff = Math.abs(len1 - len2);
+  
+  if (diff === 0) {
+    while (l1 !== null && l2 !== null) {
+      if (l1 === l2) {
+        return l1;
+      }
+      l1 = l1.next;
+      l2 = l2.next;
+    }
+    return null;
+  } 
+  else {
+    let longList = len1 > len2 ? l1 : l2;
+    let p2 = (longList === l1 ? l2 : l1);
+    while (diff > 0) {
+      longList = longList.next;
+      diff--;
+    }
+    while (longList !== null) {
+      if (longList === p2) {
+        return longList;
+      }
+      longList = longList.next;
+      p2 = p2.next;
+    }
+    return null;
+  }
+}
+
+const CTCIintersection = (l1, l2) => {
+  // Get size and tail
+  let result1 = length(l1);
+  let result2 = length(l2);
+  let diff = Math.abs(result1.counter - result2.counter);
+
+  // If different tail nodes, no intersection
+  if (result1.l1 !== result2.l1) {
+    return null;
+  }
+
+  // Set pointers to start of each linked list
+  let shorter = result1.counter < result2.counter ? l1 : l2;
+  let longer = result1.counter < result2.counter ? l2 : l1;
+
+  while (diff > 0) {
+    longer = longList.next;
+    diff--;
+  }
+
+  while (shorter !== longer) {
+    shorter = shorter.next;
+    longer = longer.next;
+  }
+
+  return longer;
 }
 
 const { LinkedListNode } = require('./list');
@@ -43,3 +117,4 @@ listTwo.next = new LinkedListNode(2);
 listTwo.next.next = common;
 
 console.log(intersection(listOne, listTwo));
+console.log(intsec(listOne, listTwo));
