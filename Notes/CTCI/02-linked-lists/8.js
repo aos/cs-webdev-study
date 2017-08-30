668ad0d035b3fe192ab5a83aab28693499c19454
Aos <aosdab@gmail.com>
2017-08-30 15:57:23 -0400

    Finished 2.8

--- Notes/CTCI/02-linked-lists/8.js
+++ Notes/CTCI/02-linked-lists/8.js
@@ -1,21 +1,40 @@
 /** 2.8 - Loop Detection
  *
  * Given a circular linked list, implement an algorithm that returns
- * the node at the beginning of the loop.
+ * the node at the ~beginning of the loop~.
  *
  * EXAMPLE
  * Input: A -> B -> C -> D -> E -> C (same C)
  * Output: C
 */
 
-const detectLoop = (node) => {
-  let p1 = node;
-  let p2 = node.next.next;
+const detectLoop = (head) => {
+  let slow = head;
+  let fast = head;
 
-  while (p1.data !== p2.data) {
-    p1 = p1.next;
-    p2 = p2.next.next;
+  // Find meeting point. This will be 'LOOP_SIZE - k' steps into LL
+  while (fast !== null & fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) { // Collision
+      break;
+    }
   }
 
-  return p1.data;
+  // Error check - no meeting point, and therefore no loop
+  if (fast === null || fast.next === null) {
+    return null
+  }
+
+  /* Move 'slow' to head. Keep 'fast' at meeting point. Each are 'k' steps
+   * from the loop start. If they move at the same pace, they must meet
+   * at loop start. */
+  slow = head;
+  while (slow !== fast) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  // Both now at start of loop
+  return fast;
 }
