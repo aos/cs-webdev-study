4801bad3501aaaa5580864b0f96a970a2997ea1f
Aos <aosdab@gmail.com>
2017-09-07 03:26:01 -0400

    ETTM and starting 3.5

---
+++ Notes/CTCI/03-stacks-queues/5.js
@@ -0,0 +1,14 @@
+/** 3.5 -- Sort Stack
+ *
+ * Write a program to sort a stack such that the smallest items are on the top.
+ * You can use an additional temporary stack, but you may not copy the elements
+ * into any other data strcuture (such as an array). The stack supports the
+ * following operations: 'push', 'pop', 'peek', 'isEmpty'.
+*/
+
+const Stack = require('./stack');
+
+function sortStack(stack) {
+  const tempStack = new Stack();
+  
+}
--- Notes/CTCI/03-stacks-queues/stack.js
+++ Notes/CTCI/03-stacks-queues/stack.js
@@ -1,14 +1,19 @@
 module.exports = function Stack() {
   const items = [];
 
+  // Push an item to the top of the stack
   this.push = function(data) {
     items.push(data);
   }
 
+  // Return item at the top of the stack
   this.pop = function() {
+    if (this.isEmpty()) return null;
+
     return items.pop();
   }
 
+  // Look at item at the top of the stack
   this.peek = function() {
     return items[items.length - 1];
   }
