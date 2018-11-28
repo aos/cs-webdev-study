3aa5df86fba3f7717b8853e416cb524c79aecaf3
Aos <aosdab@gmail.com>
2018-11-28 15:32:44 -0500

    Finished 08 - loop detection

--- Notes/CTCI/02-linked-lists/py/08.py
+++ Notes/CTCI/02-linked-lists/py/08.py
@@ -3,18 +3,50 @@
 # the beginning of the loop
 
 
-from lst import create_list, append
+from lst import Node, create_list, pick_node
 
 
-def loops(head):
+def detect_loop(head):
     slow = head
     fast = head
+    p2 = None
 
-    while slow and fast and fast.next:
+    while slow and fast:
         slow = slow.next
-        fast = fast.next.next
+        fast = fast.next and fast.next.next
 
-        if slow == fast:
-            return slow
+        # We detected a loop since the fast pointer and slow pointer have met
+        if slow.data == fast.data:
+            p2 = slow
+            break
 
+    # Assign a pointer at meeting point. Move head pointer and this pointer
+    # at the same speed until they meet. This is the start of the loop
+    while p2:
+        if head.data == p2.data:
+            p2.next = None  # Guard against infinite loop when returning
+            return p2
+
+        head = head.next
+        p2 = p2.next
+
+    # No loop as slow has fallen off and we never assigned p2
     return False
+
+
+# Test 1
+my_l = create_list([1, 2, 3, 4, 5, 6, 7, 8, 9])
+start = pick_node(my_l, 5)
+loop_node = pick_node(my_l, 9)
+# Create the loop
+loop_node.next = start
+assert(Node(5) == start)
+
+# Test 2
+s_l = create_list([1, 2, 3, 4, 5, 6])
+start_2 = pick_node(s_l, 2)
+loop_node_2 = pick_node(s_l, 6)
+# Create loop
+loop_node_2.next = start_2
+assert(Node(2) == start_2)
+print('All tests passed!')
